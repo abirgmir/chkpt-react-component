c1d39f450c620244dc126aad8e23958330600e0e
abirgmir <abirgmir@gmail.com>
2022-04-03 17:11:31 +0200

    chkpt-react-component

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Address() {
+    return (
+        <div>Address
+            <h3> Park House, Sandringham, Norfolk, England </h3>
+        </div>
+    )
+}
+
+export default Address
---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const ProfilePhoto = () => {
+    return (
+        <div className='profilePhoto'>
+        <img src='https://media.vanityfair.fr/photos/6155d4bcd2da0924ed93a32c/16:9/w_2560%2Cc_limit/GettyImages-79730198.jpeg'
+        alt='profilePhoto' />
+
+        </div>
+    )
+}
+
+export default ProfilePhoto
